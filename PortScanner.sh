c4b9a10b9da38bc9b6312d466164cf42dda2d086
Mercenario <83768808+SnowPerfectDev@users.noreply.github.com>
2023-08-12 00:18:48 -0300

    Update PortScanner.sh

--- PortScanner.sh
+++ PortScanner.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Define ANSI color codes
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+GREEN='\e[0;32m'
+RED='\e[0;31m'
+YELLOW='\e[1;33m'
+NC='\e[0m' # No Color
 
 # Function to display help
 show_help() {
-    echo "Usage: $0 -h <host_file> -p <port> -s <scan_type>"
-    echo "Options:"
-    echo "  -h : File containing list of hosts"
-    echo "  -p : Port to be scanned"
-    echo "  -s : Scan type (TCP, SYN, UDP)"
+    echo -e "Usage: $0 -h <host_file> -p <port> -s <scan_type>"
+    echo -e "Options:"
+    echo -e "  -h : File containing list of hosts"
+    echo -e "  -p : Port to be scanned"
+    echo -e "  -s : Scan type (TCP, SYN, UDP)"
 }
 
 # Function to handle argument errors
 handle_argument_error() {
-    echo "Error: Insufficient arguments."
+    echo -e "${RED}Error: Insufficient arguments.${NC}"
     show_help
     exit 1
 }
@@ -25,6 +26,8 @@ handle_argument_error() {
 port=""
 scan_type="T"  # Set to "T" for TCP Connect scan
 host_file="hosts.txt"
+scan_speed="3" # Set the desired scan speed level (1-5)
+max_hosts=3    # Maximum number of hosts to be scanned
 
 # Check for arguments
 if [ $# -eq 0 ]; then
@@ -43,13 +46,13 @@ done
 
 # Check dependency
 if ! command -v nmap &>/dev/null; then
-    echo "Nmap is not installed. Please install it first."
+    echo -e "${RED}Nmap is not installed. Please install it first.${NC}"
     exit 1
 fi
 
 # Check existence of host file
 if [ ! -f "$host_file" ]; then
-    echo "Hosts file '$host_file' not found."
+    echo -e "${RED}Hosts file '$host_file' not found.${NC}"
     exit 1
 fi
 
@@ -57,8 +60,30 @@ fi
 echo -e "${YELLOW}----------------- Scan Results -----------------${NC}"
 
 # Loop through hosts and perform scans
-while IFS= read -r host; do
-    echo -e "${GREEN}Scanning $host on port $port using $scan_type scan${NC}"
-    nmap -sT -p "$port" "$host" | awk '/^PORT/{flag=1} flag; /^$/{flag=0}' | grep "open"
-    echo -e "${YELLOW}-------------------------------${NC}\n"
+host_count=0
+while IFS= read -r host || [ -n "$host" ]; do
+    if [ "$host_count" -ge "$max_hosts" ]; then
+        break
+    fi
+
+    if [ -n "$host" ]; then
+        echo -e "${GREEN}Scanning $host on port $port using $scan_type scan${NC}"
+        nmap_result=$(nmap -T$scan_speed -sT -p "$port" "$host")
+        open_ports=$(echo "$nmap_result" | awk '/^PORT/{flag=1} flag; /^$/{flag=0}' | grep "open")
+        closed_ports=$(echo "$nmap_result" | awk '/^PORT/{flag=1} flag; /^$/{flag=0}' | grep -E "closed|filtered")
+
+        if [ -n "$open_ports" ]; then
+            echo -e "${GREEN}Open Ports:${NC}"
+            echo "$open_ports"
+        fi
+
+        if [ -n "$closed_ports" ]; then
+            echo -e "${RED}Closed/Filtered Ports:${NC}"
+            echo "$closed_ports"
+        fi
+
+        echo -e "${YELLOW}-------------------------------${NC}\n"
+        
+        ((host_count++))
+    fi
 done < "$host_file"
